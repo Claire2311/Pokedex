207c6cc253f752845f75a02a1f5ea185e87fe43e
Claire2311 <claire.girard@ieseg.fr>
2023-05-31 11:21:56 +0200

    quête 03

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,27 @@
 function PokemonCard() {
-  return (
-    <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbasaur"
-      />
-      <figcaption>Bulbausar</figcaption>
-    </figure>
-  );
+  const pokemon = pokemonList[0];
+
+  if (pokemon.imgSrc != null) {
+    return (
+      <figure>
+        <img src={pokemon.imgSrc} />
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    );
+  } else {
+    return <p>???</p>;
+  }
 }
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 export default PokemonCard;
