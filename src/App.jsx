d8e408990147267a2912d91c84fb3fee2e453641
Claire2311 <claire.girard@ieseg.fr>
2023-06-01 17:57:49 +0200

    simple reac

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
+import PropTypes from "prop-types";
 
 const pokemonList = [
   {
@@ -11,23 +10,62 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
-PokemonCard.propTypes = {
-  pokemonList: PropTypes.arrayof({
-    name: PropTypes.string.isRequired,
-    imgSrc: PropTypes.string.isRequired,
-  }).isRequired,
-};
+// PokemonCard.propTypes = {
+//   pokemonList: PropTypes.arrayof({
+//     name: PropTypes.string.isRequired,
+//     imgSrc: PropTypes.string.isRequired,
+//   }).isRequired,
+// };
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const increment = () => {
+    if (pokemonIndex - 1 < 0) {
+      setPokemonIndex = 0;
+    } else {
+      setPokemonIndex(pokemonIndex - 1);
+    }
+  };
+
+  const decrement = () => {
+    if (pokemonIndex + 1 > pokemonList.length - 1) {
+      setPokemonIndex = pokemonList.length - 1;
+    } else {
+      setPokemonIndex(pokemonIndex + 1);
+    }
+  };
 
   return (
     <div>
-      <PokemonCard {...pokemonList[0]} />
+      <div>
+        <PokemonCard
+          name={pokemonList[pokemonIndex].name}
+          imgSrc={pokemonList[pokemonIndex].imgSrc}
+        />
+      </div>
+
+      <button onClick={increment}>Précédent</button>
+      <button onClick={decrement}>Suivant</button>
     </div>
   );
 }
